1325aedf4536559a386088cd18276321a6f4e608
germainloret <germain.loret@gmail.com>
2017-05-17 12:33:01 +0200

    create seed file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian", "mexican", "thai", "moroccan", "lebanese"]
   has_many :reviews, dependent: :destroy
   validates_presence_of :name, :address, :category
   validates :category, inclusion: { in: CATEGORIES }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating Restaurant..."
+restaurants = [
+  {
+   name:          "Mamma Roma",
+   address:       "90 Rue Oberkampf, 75011 Paris, France",
+   phone_number:  "0190564689",
+   category:      "italian"
+  },
+  {
+   name:          "Bocamexa",
+   address:       "95 Rue Oberkampf, 75011 Paris, France",
+   phone_number:  "0190564685",
+   category:      "mexican"
+  },
+  {
+   name:          "Yemma",
+   address:       "119 Rue du Chemin Vert, 75011, Paris",
+   phone_number:  "0190564686",
+   category:      "moroccan"
+  },
+  {
+   name:          "Paris Hanoï",
+   address:       "74 Rue de Charonne, 75011, Paris",
+   phone_number:  "0190564687",
+   category:      "thai"
+  },
+  {
+   name:          "Zaatar",
+   address:       "43 Rue de Montreuil, 75011, Paris",
+   phone_number:  "0190564688",
+   category:      "lebanese"
+  }
+]
+
+Restaurant.create!(restaurants)
+
+puts "Finished!"
