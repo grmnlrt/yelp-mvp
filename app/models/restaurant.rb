a09c48458c3ce1078ebbbfb04b8cb3e35de39ab0
germainloret <germain.loret@gmail.com>
2017-05-17 18:36:13 +0200

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,10 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates_presence_of :name, :address, :category
   validates :category, inclusion: { in: CATEGORIES }
+
+  def average
+    ratings = self.reviews.length
+    sum = self.reviews.inject(0){|sum,review| sum + review.rating }
+    ratings != 0 ? (sum.to_f / ratings).round() : "No review yet"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+
+  # homepage
+  root 'restaurants#index'
+
+  # add a new review to a restaurant
+  resources :restaurants, only: [ :index, :new, :create, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
